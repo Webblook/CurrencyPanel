bb0aef8cb96a4df91f2e279ffb999c8bd1f68d62
Daniil Dubrava <daniil.dubrava@yandex.ru>
2018-06-06 22:19:30 +0300

    Add new widget - LastUpdate to check the last uploaded rate exchange

---
+++ src/components/widgets/WidgetLastUpdate.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12">
+    <div class="widget">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-6 align-self-center">
+            <i class="ion-md-time"></i>
+          </div>
+          <div class="col-6 align-self-center">
+            <h1>{{ this.date.years[6]+'/'+this.date.months[6]+'/'+this.date.dates[6] }}</h1>
+            <p>Last update</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    dataCurrencies: {
+      type: Object,
+      required: true
+    },
+
+    date: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang='sass' scoped>
+h1
+  font-size: 2.5rem
+</style>
