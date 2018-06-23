b1950b0778115f75a64ed1e80bc9eda5277d7bbc
Daniil Dubrava <daniil.dubrava@yandex.ru>
2018-06-23 15:36:01 +0300

    configure postcss in webpack config, remove .scss

--- webpack.config.js
+++ webpack.config.js
@@ -19,18 +19,11 @@ module.exports = {
         ]
       },
       {
-        test: /\.scss$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader'
-        ],
-      },
-      {
         test: /\.sass$/,
         use: [
           'vue-style-loader',
           'css-loader',
+          'postcss-loader',
           'sass-loader?indentedSyntax'
         ],
       },
@@ -42,14 +35,10 @@ module.exports = {
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': [
-              'vue-style-loader',
-              'css-loader',
-              'sass-loader'
-            ],
             'sass': [
               'vue-style-loader',
               'css-loader',
+              'postcss-loader',
               'sass-loader?indentedSyntax'
             ]
           }
